f77860fe3d8e101e2a5df53a84d78d3e055394c9
eleves <eleves@mac404inf-09.local>
2017-08-10 09:59:37 -0400

    1st commit after build and run

--- MadWatch WatchKit Extension/InterfaceController.swift
+++ MadWatch WatchKit Extension/InterfaceController.swift
@@ -1,31 +1,82 @@
-//
-//  InterfaceController.swift
-//  MadWatch WatchKit Extension
-//
-//  Created by eleves on 2017-08-10.
-//  Copyright © 2017 wd. All rights reserved.
-//
-
+//======================================================
 import WatchKit
 import Foundation
-
-
-class InterfaceController: WKInterfaceController {
-
+import WatchConnectivity
+//======================================================
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    //--------------------------------------------------
+    @IBOutlet var messageLabel: WKInterfaceLabel!
+    @IBOutlet var sendButton: WKInterfaceButton!
+    //-----------
+    var session : WCSession!
+    //-----------
+    var conversation: String = ""
+    //--------------------------------------------------
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
     }
-    
+    //--------------------------------------------------
     override func willActivate() {
-        // This method is called when watch view controller is about to be visible to user
+        //-----------
         super.willActivate()
+        //-----------
+        if WCSession.isSupported() {
+            session = WCSession.default()
+            session.delegate = self
+            session.activate()
+        }
+        //-----------
     }
-    
+    //--------------------------------------------------
     override func didDeactivate() {
-        // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
-
+    //--------------------------------------------------
+    @IBAction func sendToParent() {
+        sendMessage()
+    }
+    //--------------------------------------------------
+    @available(watchOS 2.2, *)
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //..code
+    }
+    //--------------------------------------------------
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        //-----------
+        // Message du téléphone
+        let value = message["Message"] as? String
+        conversation += value!
+        //-----------
+        DispatchQueue.main.async { () -> Void in
+            // Affichage sur montre
+            self.messageLabel.setText(self.conversation)
+        }
+        //-----------
+        replyHandler(["Message" : conversation])
+        //-----------
+    }
+    //--------------------------------------------------
+    func sendMessage() {
+        presentTextInputController(withSuggestions: ["Bonjour"], allowedInputMode: WKTextInputMode.plain,
+                                   completion: {(results) -> Void in
+                                    if results != nil && results!.count > 0 {
+                                        let aResult = results?[0] as? String
+                                        self.conversation += "Watch : \(aResult!)\n\n"
+                                        self.messageLabel.setText(self.conversation)
+                                        self.session.sendMessage(["Message" : self.conversation],
+                                                                 replyHandler: {replyMessage in}, errorHandler: {error in print(error)})
+                                    }
+        })
+    }
+    //--------------------------------------------------
 }
+//======================================================
+
+
+
+
+
+
+
+
+
--- MadWatch/ViewController.swift
+++ MadWatch/ViewController.swift
@@ -1,25 +1,101 @@
-//
-//  ViewController.swift
-//  MadWatch
-//
-//  Created by eleves on 2017-08-10.
-//  Copyright © 2017 wd. All rights reserved.
-//
-
+//======================================================
 import UIKit
-
-class ViewController: UIViewController {
-
+import WatchConnectivity
+//======================================================
+class ViewController: UIViewController, WCSessionDelegate {
+    //--------------------------------------------------
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var messageField: UITextField!
+    @IBOutlet weak var replyLabel: UITextView!
+    //-----------
+    var session: WCSession!
+    //-----------
+    var conversation: String = ""
+    //--------------------------------------------------
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //-----------
+        if WCSession.isSupported() {
+            session = WCSession.default()
+            session.delegate = self
+            session.activate()
+        }
+        //-----------
     }
-
+    //--------------------------------------------------
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
-
+    //--------------------------------------------------
+    @available(iOS 9.3, *)
+    public func sessionDidDeactivate(_ session: WCSession) {
+        //..
+    }
+    //--------------------------------------------------
+    @available(iOS 9.3, *)
+    public func sessionDidBecomeInactive(_ session: WCSession) {
+        //..
+    }
+    //--------------------------------------------------
+    @available(iOS 9.3, *)
+    public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+        //..
+    }
+    //--------------------------------------------------
+    @IBAction func sendToWatch(_ sender: AnyObject) {
+        sendMessage()
+    }
+    //--------------------------------------------------
+    func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+        //-----------
+        // Message envoyé par la montre
+        let value = message["Message"] as? String
+        //-----------
+        DispatchQueue.main.async { () -> Void in
+            // Affichage sur téléphone
+            self.replyLabel.text = value
+        }
+        //-----------
+        // Message automatique envoyé à la montre
+        // replyHandler(["Message" : "Un message"])
+        //-----------
+    }
+    //--------------------------------------------------
+    func sendMessage(){
+        //-----------
+        // Message envoyé par le téléphone
+        let messageToSend = ["Message" : "Phone : \(messageField.text!)\n\n"]
+        //-----------
+        session.sendMessage(messageToSend, replyHandler: { (replyMessage) in
+            //-----------
+            // Message automatique de la montre
+            let value = replyMessage["Message"] as? String
+            //-----------
+            DispatchQueue.main.async(execute: { () -> Void in
+                // Affichage sur téléphone
+                self.replyLabel.text = value
+                self.messageField.text = ""
+            })
+            //-----------
+        }) { (error) in
+            print("error: \(error.localizedDescription)")
+        }
+        //-----------
+    }
+    //--------------------------------------------------
 }
+//======================================================
+
+
+
+
+
+
+
+
+
+
+
+
+
 
